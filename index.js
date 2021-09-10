f1d3bb8052cbeea2c6a734d670813baaad7449fd
Christopher <christopher.diercksen@gmail.com>
2021-09-10 16:48:12 -0400

    commit tree 0575df43fbda80f3e9d4b149ed2aab52f9b32ccf

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(input = 2000) {
+    return input / 100;
+}
+
+const square = (parameter) => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
